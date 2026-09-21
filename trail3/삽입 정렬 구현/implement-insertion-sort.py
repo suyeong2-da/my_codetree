3366eb2b016edb896fab4d7fd46d7d1e14b1af38
suyeong2 <suyeong2.da@gmail.com>
2026-09-21 16:31:50 +0900

    [Passed] 삽입 정렬 구현 (48ms, 16MB)

---
+++ trail3/삽입 정렬 구현/implement-insertion-sort.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+for i in range(1, n):
+    j = i-1
+    key = arr[i]
+    while j >=0 and arr[j]>key:
+        arr[j+1] = arr[j]
+        j -=1
+    arr[j+1] = key
+
+print(*arr)
